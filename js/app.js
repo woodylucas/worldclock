61d24d59bac0c1b4c6130b6c547c6e97c5726a41
Woody <WLucas0311@gmail.com>
2018-10-12 16:58:52 -0400

    fixing the times

--- js/app.js
+++ js/app.js
@@ -4,11 +4,14 @@ const fullDate = new Date();
 var hours = fullDate.getHours();
 var mins = fullDate.getMinutes();
 var secs = fullDate.getSeconds();
-var egypt = hours + 6 - 24 + 24;
-var china = hours + 12 - 24 + 24;
-var australia = hours + 14 - 24 + 24;
-var brazil = hours + 1 - 24 + 24;
-var uk = hours + 5 - 24 + 24;
+
+egypt = hours + 6 - 24 + 24;
+var china = hours - 12 ;
+var australia = hours + 14;
+var brazil = hours + 1 ;
+var uk = hours + 5;
+
+
 
 
 //To place a zero in front of the clock, conditional statment.
@@ -20,6 +23,36 @@ if (mins < 10 ) {
 }
 if (secs < 10 ) {
   secs = "0" + secs;
+}
+if (egypt < 10 ) {
+  egypt = "0" + egypt;
+}
+if (egypt === 24) {
+  egypt = 0;
+}
+if (china < 10 ) {
+  china = "0" + china;
+}
+if (china === 24) {
+  china = 0;
+}
+if (australia < 10 ) {
+  australia = "0" + australia;
+}
+if (australia === 24) {
+  australia = 0;
+}
+if (brazil < 10 ) {
+   brazil = "0" + brazil;
+}
+if (brazil === 24) {
+  brazil = 0;
+}
+if (uk < 10 ) {
+   uk = "0" + uk;
+}
+if (uk === 24) {
+  uk = 0;
 }
 
 
